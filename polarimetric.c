8c8332ad6e6449f21cfbf5c2b36587bb34ed9341
gsargent10-psoas <garrett.sargent@psoassolutions.com>
2022-01-03 21:57:04 -0500

    Added formugrid function.

--- polarimetric.c
+++ polarimetric.c
@@ -2,14 +2,113 @@
 #include<math.h>
 #include<stdlib.h>
 
-/*** Global Variables ***/
-
-
 /*** Internal Functions ***/
 
 
 /*** External Functions ***/
-int formugrid(double *in_data, double *out_data, double *pattern, int num_patterns)
+
+/* input: modulation={0,1,2,3} (2x2a,2x2b,3x2,4x2); */
+int formugrid(double *s0, double *s1, double *s2, double *out_data, int width, int height, int modulation)
 {
-	return 0;
+	if (modulation > 3 || modulation < 0)
+		return -1;
+
+	double a22a[4] = {0.0,45.0,135.0,90.0};
+	double a22b[4] = {22.5,67.5,112.5,157.5};
+	double a32[6] = {0.0,120.0,60.0,90.0,30.0,150.0};
+	double a42[8] = {0.0,45.0,90.0,135.0,90.0,135.0,0.0,45.0};
+	for(int k=0;k<4;k++)
+	{
+		a22a[k]=2.0*a22a[k]/(M_PI/180.0);
+		a22b[k]=2.0*a22b[k]/(M_PI/180.0);
+	}
+	for(int k=0;k<6;k++)
+	{
+		a32[k]=2.0*a32[k]/(M_PI/180.0);
+	}
+	for(int k=0;k<8;k++)
+	{
+		a42[k]=2.0*a42[k]/(M_PI/180.0);
+	}
+	
+	int indx=0;	
+	for(int r=0;r<height;r++)
+	{
+		for(int c=0;c<width;c++)
+		{
+			indx=c+r*width;
+			if(modulation==0) // 2x2a, top-left=90, top-right=135, bottom-left=45, bottom-right=0
+			{
+				if(r%2==1 && c%2==1) // 0, bottom-right
+					out_data[indx] = 0.5*(s0[indx]+cos(a22a[0])*s1[indx] + sin(a22a[0])*s2[indx]); 	
+				if(r%2==1 && c%2==0) // 45, bottom-left
+					out_data[indx] = 0.5*(s0[indx]+cos(a22a[1])*s1[indx] + sin(a22a[1])*s2[indx]); 	
+				if(r%2==0 && c%2==1) // 135, top-right
+					out_data[indx] = 0.5*(s0[indx]+cos(a22a[2])*s1[indx] + sin(a22a[2])*s2[indx]); 	
+				if(r%2==0 && c%2==0) // 90, top-left
+					out_data[indx] = 0.5*(s0[indx]+cos(a22a[3])*s1[indx] + sin(a22a[3])*s2[indx]); 	
+			}
+			else if(modulation==1) // 2x2b, top-left=22.5, top-right=67.5, bottom-left=112.5, bottom-right=157.5
+			{
+				if(r%2==1 && c%2==1) // 157.5, bottom-right
+					out_data[indx] = 0.5*(s0[indx]+cos(a22b[3])*s1[indx] + sin(a22b[3])*s2[indx]); 	
+				if(r%2==1 && c%2==0) // 112.5, bottom-left
+					out_data[indx] = 0.5*(s0[indx]+cos(a22b[2])*s1[indx] + sin(a22b[2])*s2[indx]); 	
+				if(r%2==0 && c%2==1) // 67.5, top-right
+					out_data[indx] = 0.5*(s0[indx]+cos(a22b[1])*s1[indx] + sin(a22b[1])*s2[indx]); 	
+				if(r%2==0 && c%2==0) // 22.5, top-left
+					out_data[indx] = 0.5*(s0[indx]+cos(a22b[0])*s1[indx] + sin(a22b[0])*s2[indx]); 	
+			}
+			else if(modulation==2) // 3x2
+			{
+				if(r%2==1 && c%3==1)
+					out_data[indx] = 0.5*(s0[indx]+cos(a32[5])*s1[indx] + sin(a32[5])*s2[indx]); 	
+				if(r%2==1 && c%3==2)
+					out_data[indx] = 0.5*(s0[indx]+cos(a32[4])*s1[indx] + sin(a32[4])*s2[indx]); 	
+				if(r%2==1 && c%3==0)
+					out_data[indx] = 0.5*(s0[indx]+cos(a32[3])*s1[indx] + sin(a32[3])*s2[indx]); 	
+				if(r%2==0 && c%3==1)
+					out_data[indx] = 0.5*(s0[indx]+cos(a32[2])*s1[indx] + sin(a32[2])*s2[indx]); 	
+				if(r%2==0 && c%3==2)
+					out_data[indx] = 0.5*(s0[indx]+cos(a32[1])*s1[indx] + sin(a32[1])*s2[indx]); 	
+				if(r%2==0 && c%3==0)
+					out_data[indx] = 0.5*(s0[indx]+cos(a32[0])*s1[indx] + sin(a32[0])*s2[indx]); 	
+			}
+			else if(modulation==3) // 4x2
+			{
+				if(r%2==1 && c%4==1)
+					out_data[indx] = 0.5*(s0[indx]+cos(a42[7])*s1[indx] + sin(a42[7])*s2[indx]); 	
+				if(r%2==1 && c%4==2)
+					out_data[indx] = 0.5*(s0[indx]+cos(a42[6])*s1[indx] + sin(a42[6])*s2[indx]); 	
+				if(r%2==1 && c%4==3)
+					out_data[indx] = 0.5*(s0[indx]+cos(a42[5])*s1[indx] + sin(a42[5])*s2[indx]); 	
+				if(r%2==1 && c%4==0)
+					out_data[indx] = 0.5*(s0[indx]+cos(a42[4])*s1[indx] + sin(a42[4])*s2[indx]); 	
+				if(r%2==0 && c%4==1)
+					out_data[indx] = 0.5*(s0[indx]+cos(a42[3])*s1[indx] + sin(a42[3])*s2[indx]); 	
+				if(r%2==0 && c%4==2)
+					out_data[indx] = 0.5*(s0[indx]+cos(a42[2])*s1[indx] + sin(a42[2])*s2[indx]); 	
+				if(r%2==0 && c%4==3)
+					out_data[indx] = 0.5*(s0[indx]+cos(a42[1])*s1[indx] + sin(a42[1])*s2[indx]); 	
+				if(r%2==0 && c%4==0)
+					out_data[indx] = 0.5*(s0[indx]+cos(a42[0])*s1[indx] + sin(a42[0])*s2[indx]); 	
+			}
+			else
+			{
+				return -1;
+			}
+		}
+	}
+	return modulation;
 }
+
+/* input: in_data= ; out_data=height by width with depth of 5 (so,s1,s2,dolp,aop) */
+void computestokes(double *in_data, double *out_data, int width, int height, double *pattern, int num_patterns)
+{
+	int s0=1; int s1=1; int s2=2; int dolp=3; int aop=4;
+	for(int k=0;k<height*width;k++)
+	{
+		//out_data[k+s0*width*height] = 
+	}
+}
+
--- polarimetric.h
+++ polarimetric.h
@@ -1,2 +1,4 @@
 /**/
-int formugrid(double *in_data, double *out_data, double *pattern, int num_patterns);
+int formugrid(double *s0, double *s1, double *s2, double *out_data, int width, int height, int modulation);
+
+void computestokes(double *in_data, double *out_data, int width, int height, double *pattern, int num_patterns);
