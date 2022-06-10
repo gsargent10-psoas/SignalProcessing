bc136da3e55058e5467e3431d4da7fb4ee8763cb
Alex McDaniel <alexmcdaniel2168@gmail.com>
2022-06-10 09:45:30 -0400

    Saved Stitching Function in .h file

--- src/polarimetric/polarimetric.h
+++ src/polarimetric/polarimetric.h
@@ -5,3 +5,6 @@ int formugrid(double *s0, double *s1, double *s2, double *out_data, int width, i
 /* Compute intensity images from Stokes.
 input: in_data= ; out_data=height by width with depth of 5 (so,s1,s2,dolp,aop) */
 void computeintensity(double *s0,double *s1, double *s2, double *out_data, int width, int height, double angle);
+
+/* The stitching algroithim */
+void Stitching();
