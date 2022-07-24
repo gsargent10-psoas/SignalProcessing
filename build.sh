0c33c59383144921e96135692683f79bda6adb15
gsargent10-psoas <garrett.sargent@psoassolutions.com>
2022-07-24 14:12:09 -0400

    Added a build script to streamline the build process.

---
+++ build.sh
@@ -0,0 +1,48 @@
+#! /usr/bin/bash
+
+# Maintain pointer to current directory.
+current_d=$(pwd)
+
+# Colors: black=30;red=31;green=32;yellow=33;blue=34;magenta=35;cyan=36;white=37; \e[1;<num>m]
+
+# Start the build process and inform the user of the tests to be performed.
+greeting="Building all of SignalProcessing..."
+build_list="Build List:"
+echo -e $greeting
+echo -e $build_list
+
+# Get all nested directories that have a build.sh.
+count=1
+for d in $(find ./src -maxdepth 1 -type d)
+do
+    echo -e "\t\e[1;34m$count. $d"
+    let count=count+1
+done
+
+# Run all build.sh and get results.
+count=1
+retn_count=0
+for d in $(find ./src -maxdepth 1 -type d)
+do
+    echo -e "\e[1;37m\nBuilding \e[1;34m$d\e[1;37m..."
+    cd $d
+    ./build.sh
+    retn=$?
+    if [ $retn==0 ]
+    then
+        echo -e "\e[1;32m\n$d Build Succeeded!\e[1;37m"
+    else
+        echo -e "\e[1;31m\n$d Build Failed!\e[1;37m"
+    fi
+    let retn_count=retn_count+$retn
+    cd $current_d
+    let count=count+1
+done
+
+# Print out results.
+if [ $retn_count==0 ]
+then
+    echo -e "\e[1;32m\nAll Builds Succeeded!\e[1;37m"
+else
+    echo -e "\e[1;31m\nBuild(s) Failed!\e[1;37m"
+fi
